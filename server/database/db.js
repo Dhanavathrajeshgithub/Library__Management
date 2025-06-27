d4f92e6ead02fa345f48e26ac6188ae7bdd3fcd5
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-06-27 09:21:53 +0530

    Adding backend first time

---
+++ server/database/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "MERN LIBRARY MANAGEMENT SYSTEM",
+    })
+    .then(() => {
+      console.log("DataBase connected successfully");
+    })
+    .catch((err) => {
+      console.log("Error connecting DB", err);
+    });
+};
