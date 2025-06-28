7df45af194469e7eba7a89c8ff3c0a513772b063
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-06-28 07:21:36 +0530

    Furthur improvements in backend

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "MERN LIBRARY MANAGEMENT SYSTEM",
+      dbName: "MERN_LIBRARY_MANAGEMENT_SYSTEM",
     })
     .then(() => {
       console.log("DataBase connected successfully");
